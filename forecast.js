afd81c5d18647157e3a4aad692324d6bef81617a
John Wade <johnw8017@gmail.com>
2021-04-08 07:47:41 -0400

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -19,9 +19,13 @@ puppeteer.launch({
         let ForecastData = [];
         allPosts.forEach(item => {
             let Forecast = {};
-		 
+	
+	// This is only returning the 1st item.  I've tried .map, append, a regular loop.... can only ever get the 1st item
+	// so the first nth child selector is returning from 1-6 [really 0-6] the second nth child is always the same
+	// it gets the 1st one but does not get the rest of them... 
+		
             Forecast.Day = item.querySelector("div.forecast-date > a:nth-child(-n+6) > div > div").innerHTML;
-	        Forecast.Icon = item.querySelector("div.forecast > a:nth-child(-n+6) > div > span:nth-child(2) > img").getAttribute('src');
+	    Forecast.Icon = item.querySelector("div.forecast > a:nth-child(-n+6) > div > span:nth-child(2) > img").getAttribute('src');
             Forecast.High = item.querySelector("div.forecast > a:nth-child(-n+6) > div > span:nth-child(1) > span.temp-hi").innerHTML;
             Forecast.Low = item.querySelector("div.forecast > a:nth-child(-n+6) > div > span:nth-child(1) > span.temp-lo").innerHTML;
             Forecast.Desc = item.querySelector("div.forecast > a:nth-child(-n+6) > div > div").innerHTML;
