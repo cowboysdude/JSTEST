d64d9654805a7aad86895bb3c3f07d3de8f3e951
John Wade <johnw8017@gmail.com>
2021-04-09 18:19:43 -0400

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -21,15 +21,15 @@ puppeteer.launch({
         allPosts.forEach(item => {
             
 	//using querySelectorAll on the below elements returns nothing.......example:
-	//Forecast.Day = item.querySelectorAll("div.forecast-date > a:nth-child(-n+6) > div > div").innerHTML;
-		// still only returns the 1st item, not the next the 5......
+	//Forecast.Day = item.querySelectorAll("div.forecast-date > a:nth-child(-n+4) > div > div").innerHTML;
+		// still only returns the 1st item, not the next the 3......
 			
-	    Forecast.Day = item.querySelector("div.forecast-date > a:nth-child(-n+6) > div > div").innerHTML;
-	    Forecast.Icon = item.querySelector("div.forecast > a:nth-child(-n+6) > div > span:nth-child(2) > img").getAttribute('src');
-            Forecast.High = item.querySelector("div.forecast > a:nth-child(-n+6) > div > span:nth-child(1) > span.temp-hi").innerHTML;
-            Forecast.Low = item.querySelector("div.forecast > a:nth-child(-n+6) > div > span:nth-child(1) > span.temp-lo").innerHTML;
-            Forecast.Desc = item.querySelector("div.forecast > a:nth-child(-n+6) > div > div").innerHTML;
-	    Forecast.Rain = item.querySelector("div.precip > a:nth-child(-n+6) > div > div > span").innerHTML;
+	    Forecast.Day = item.querySelector("div.forecast-date > a:nth-child(-n+4) > div > div").innerHTML;
+	    Forecast.Icon = item.querySelector("div.forecast > a:nth-child(-n+4) > div > span:nth-child(2) > img").getAttribute('src');
+            Forecast.High = item.querySelector("div.forecast > a:nth-child(-n+4) > div > span:nth-child(1) > span.temp-hi").innerHTML;
+            Forecast.Low = item.querySelector("div.forecast > a:nth-child(-n+4) > div > span:nth-child(1) > span.temp-lo").innerHTML;
+            Forecast.Desc = item.querySelector("div.forecast > a:nth-child(-n+4) > div > div").innerHTML;
+	    Forecast.Rain = item.querySelector("div.precip > a:nth-child(-n+4) > div > div > span").innerHTML;
 		   
         });
 		 
