ecf9a94a70c0f44fa55cc9bcccb56e685b7e318c
John Wade <johnw8017@gmail.com>
2021-04-09 06:57:03 -0400

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -23,21 +23,21 @@ puppeteer.launch({
 	
 	// This is only returning the 1st item.  I've tried .map, append, a regular loop.... can only ever get the 1st item
 	// so the first nth child selector is returning from 1-6 [really 0-6] the second nth child is always the same
-	// it gets the 1st one but does not get the rest of them... 
+	 
 		
-	// MY return //
+	// this is what it's returning..... so below it cycles and gets this:
 	// [{"Day":"Thu 4/8","Icon":"//www.wunderground.com/static/i/c/v4/34.svg","High":"73°","Low":"50°","Desc":"Mostly Sunny","Rain":"0 in"}] 	
 	// should be:
 	// [{"Day":"Thu 4/8","Icon":"//www.wunderground.com/static/i/c/v4/34.svg","High":"73°","Low":"50°","Desc":"Mostly Sunny","Rain":"0 in"}
 	//  {"Day":"Fri 4/9","Icon":"//www.wunderground.com/static/i/c/v4/32.svg","High":"63°","Low":"50°","Desc":"Mostly Sunny","Rain":"0 in"}
 	//     .... and so on until the 5th child is reached. ]
 		
-            Forecast.Day = item.querySelector("div.forecast-date > a:nth-child(-n+6) > div > div").innerHTML;
-	    Forecast.Icon = item.querySelector("div.forecast > a:nth-child(-n+6) > div > span:nth-child(2) > img").getAttribute('src');
-            Forecast.High = item.querySelector("div.forecast > a:nth-child(-n+6) > div > span:nth-child(1) > span.temp-hi").innerHTML;
-            Forecast.Low = item.querySelector("div.forecast > a:nth-child(-n+6) > div > span:nth-child(1) > span.temp-lo").innerHTML;
-            Forecast.Desc = item.querySelector("div.forecast > a:nth-child(-n+6) > div > div").innerHTML;
-	    Forecast.Rain = item.querySelector("div.precip > a:nth-child(-n+6) > div > div > span").innerHTML;
+            Forecast.Day = item.querySelectorAll("div.forecast-date > a:nth-child(-n+6) > div > div").innerHTML;
+	    Forecast.Icon = item.querySelectorAll("div.forecast > a:nth-child(-n+6) > div > span:nth-child(2) > img").getAttribute('src');
+            Forecast.High = item.querySelectorAll("div.forecast > a:nth-child(-n+6) > div > span:nth-child(1) > span.temp-hi").innerHTML;
+            Forecast.Low = item.querySelectorAll("div.forecast > a:nth-child(-n+6) > div > span:nth-child(1) > span.temp-lo").innerHTML;
+            Forecast.Desc = item.querySelectorAll("div.forecast > a:nth-child(-n+6) > div > div").innerHTML;
+	    Forecast.Rain = item.querySelectorAll("div.precip > a:nth-child(-n+6) > div > div > span").innerHTML;
 		  
             ForecastData.push(Forecast);
         });
