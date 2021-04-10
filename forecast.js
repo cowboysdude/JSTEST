8132abbbae9cf147f15333665f07ded45985bceb
John Wade <johnw8017@gmail.com>
2021-04-10 11:55:46 -0400

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -12,37 +12,74 @@ puppeteer.launch({
     await page.waitForSelector("body");
 
     // manipulating the page"s content
-    let grabWeather = await page.evaluate(() => {
+    ForecastData = [];
+Forecast = {};
+var Day, Icon, High, Low, Desc, Rain;
 
-        var allPosts = document.querySelectorAll("#inner-content > div.region-content-main > div:nth-child(1) > div > div:nth-child(1) > div > lib-forecast-chart > div > div");
- 
-        let ForecastData = [];
-		let Forecast = {};
-        allPosts.forEach(item => {
-            
-	//using querySelectorAll on the below elements returns nothing.......example:
-	//Forecast.Day = item.querySelectorAll("div.forecast-date > a:nth-child(-n+4) > div > div").innerHTML;
-		// still only returns the 1st item, not the next the 3......
-			
-	    Forecast.Day = item.querySelector("div.forecast-date > a:nth-child(-n+4) > div > div").innerHTML;
-	    Forecast.Icon = item.querySelector("div.forecast > a:nth-child(-n+4) > div > span:nth-child(2) > img").getAttribute('src');
-            Forecast.High = item.querySelector("div.forecast > a:nth-child(-n+4) > div > span:nth-child(1) > span.temp-hi").innerHTML;
-            Forecast.Low = item.querySelector("div.forecast > a:nth-child(-n+4) > div > span:nth-child(1) > span.temp-lo").innerHTML;
-            Forecast.Desc = item.querySelector("div.forecast > a:nth-child(-n+4) > div > div").innerHTML;
-	    Forecast.Rain = item.querySelector("div.precip > a:nth-child(-n+4) > div > div > span").innerHTML;
-		   
-        });
-		 
-		ForecastData.push(Forecast); 
-		 
-        return JSON.stringify(ForecastData);
-    });
-  
+Day = await page.evaluate((selector) => {
+  const list = document.querySelectorAll(selector);
+  const anchors = [...list];
+  return anchors.map(Day => Day.innerHTML); 
+}, 'div.forecast-date > a:nth-child(-n+5) > div > div'); 
+       
+ Icon = await page.evaluate((selector) => {
+  const list = document.querySelectorAll(selector);
+  const anchors = [...list];
+  return anchors.map(Icon => Icon.getAttribute('src'));
+}, "div.forecast > a:nth-child(-n+5) > div > span:nth-child(2) > img");
+
+ High = await page.evaluate((selector) => {
+  const list = document.querySelectorAll(selector);
+  const anchors = [...list];
+  return anchors.map(High => High.innerHTML);
+}, "div.forecast > a:nth-child(-n+5) > div > span:nth-child(1) > span.temp-hi"); 
+
+ Low = await page.evaluate((selector) => {
+  const list = document.querySelectorAll(selector);
+  const anchors = [...list];
+  return anchors.map(Low => Low.innerHTML);
+}, "div.forecast > a:nth-child(-n+5) > div > span:nth-child(1) > span.temp-lo");
+
+ Desc = await page.evaluate((selector) => {
+  const list = document.querySelectorAll(selector);
+  const anchors = [...list];
+  return anchors.map(Desc => Desc.innerHTML);
+}, "div.forecast > a:nth-child(-n+5) > div > div");
+
+ Rain = await page.evaluate((selector) => {
+  const list = document.querySelectorAll(selector);
+  const anchors = [...list];
+  return anchors.map(Rain => Rain.innerHTML);
+}, "div.precip > a:nth-child(-n+5) > div > div > span");
+
+/*for (i=0; i < Day.length; i++){
+	var days = Object.assign(Forecast, JSON.stringify(Day[i]));
+	console.log(days);
+} */
+ console.log(Day.length);
+var keys = ["day", "icon", "high", "low", "desc", "rain"];
+var values = [Day, Icon, High, Low, Desc, Rain];
+var resultArray = [];
+for(var i=0; i<values.length; i++){
+  var obj = {};
+  for(var j=0; j<keys.length; j++){
+     obj[keys[j]] = values[i][j];
+  }
+   resultArray.push(JSON.stringify(obj));
+} 
     // outputting the data
-    
-	console.log(grabWeather);
-	
-	
+    // this outputs this ->  
+   //	[
+//  '{"day":"Sat 4/10","icon":"Sun 4/11","high":"Mon 4/12","low":"Tue 4/13","desc":"Wed 4/14"}',
+//  '{"day":"//www.wunderground.com/static/i/c/v4/34.svg","icon":"//www.wunderground.com/static/i/c/v4/12.svg","high":"//www.wunderground.com/static/i/c/v4/11.svg","low":"//www.wunderground.com/static/i/c/v4/11.svg","desc":"//www.wunderground.com/static/i/c/v4/11.svg"}',
+//  '{"day":"79°","icon":"68°","high":"58°","low":"59°","desc":"59°"}',
+//  '{"day":"56°","icon":"51°","high":"46°","low":"44°","desc":"41°"}',
+//  '{"day":"Mostly Sunny","icon":"Rain","high":"Showers","low":"Showers","desc":"Showers"}',
+//  '{"day":"0.13 in","icon":"0.82 in","high":"0.32 in","low":"0.39 in","desc":"0.14 in"}'
+//]
+///  getting closer but not there quite yet.... trying to figure out how to fill out the data correctly.  as you can see it's putting all the data from Day into the 1st object, and so forth	
+  
+     console.log(resultArray); 
     //await fsp.writeFile(this.path, JSON.stringify(grabWeather, null, "\t")); 
 
     // closing the browser
